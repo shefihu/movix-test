4ed704067e49f63e65b148bc98384c20523c8a75
shefihu <rotimib0@gmail.com>
2023-01-16 20:35:06 +0100

    skeleton loader

--- src/components/Home/MovieRows.jsx
+++ src/components/Home/MovieRows.jsx
@@ -4,7 +4,7 @@ import arrowLeft from "../../assets/icons/arrowleft.png";
 import arrowRight from "../../assets/icons/arrowright.png";
 import greater from "../../assets/icons/littlegreater.png";
 import Thumnail from "./Thumnail";
-const MovieRows = ({ title, movies, unique_name }) => {
+const MovieRows = ({ title, movies, unique_name, loading }) => {
   const rowRef = useRef(null);
   const [isMove, setIsMove] = useState(false);
   const handleClick = (direction) => {
@@ -57,7 +57,18 @@ const MovieRows = ({ title, movies, unique_name }) => {
               {movies?.slice(0, 20)?.map((movie) => {
                 return (
                   <div key={movie.id}>
-                    <Thumnail movie={movie} unique_name={unique_name} />
+                    {loading ? (
+                      <div className="flex flex-col m-8 rounded shadow-md w-60 sm:w-80 animate-pulse h-96">
+                        <div className="h-48 rounded-t bg-gray-200"></div>
+                        <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 bg-gray-400">
+                          <div className="w-full h-6 rounded bg-gray-200"></div>
+                          <div className="w-full h-6 rounded bg-gray-200"></div>
+                          <div className="w-3/4 h-6 rounded bg-gray-200"></div>
+                        </div>
+                      </div>
+                    ) : (
+                      <Thumnail movie={movie} unique_name={unique_name} />
+                    )}
                   </div>
                 );
               })}
